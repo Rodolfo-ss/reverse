56a6c471797e39329365e198b727755fc52b5c8a
unknown <rodolfcs@gmail.com>
2023-09-30 08:23:42 -0300

    reverse

---
+++ reverse.rb
@@ -0,0 +1,19 @@
+arr1 = ["a", "b", "c", "d"]
+arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr3 = [1]
+arr4 = [1, 2]
+arr5 = ["Ramos", "Roger", "Shurasteley", "Shum", "Bruno Mars"]
+
+
+A = arr1.reverse!
+B = arr2.reverse!
+C = arr3.reverse!
+D = arr4.reverse!
+E = arr5.reverse!
+
+
+puts "#{A}"
+puts "#{B}"
+puts "#{C}"
+puts "#{D}"
+puts "#{E}"
